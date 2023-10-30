7d5ea70daaa91bf3a7a7f68eb627a3dbf8af446f
jaesmoris <javi.esmoris@gmail.com>
2023-10-30 12:32:25 +0100

    tests para checkLong

--- test/test_model/test_model.py
+++ test/test_model/test_model.py
@@ -1,53 +1,77 @@
 import sys
 sys.path.insert(1,'src\model')
 
-from model import checkWord
+from model import checkWord, checkLong
 # UNIT TESTING
 
 #  PALABRA DESORDENADA
 def test_checkWord_1():
-    word1 = "ROMA"
-    word2 = "AMOR"
-    bool_result, result = checkWord(word1, word2)
+    correct_word = "ROMA"
+    user_word = "AMOR"
+    bool_result, result = checkWord(correct_word, user_word)
     expected_result = ['*', '*', '*', '*']
     assert bool_result is False
     assert result == expected_result
 
 #  PALABRA PARCIALMENTE DESORDENADA
 def test_checkWord_2():
-    word1 = "AAAABBBCCD"
-    word2 = "ABCDABCABA"
-    bool_result, result = checkWord(word1, word2)
+    correct_word = "AAAABBBCCD"
+    user_word = "ABCDABCABA"
+    bool_result, result = checkWord(correct_word, user_word)
     expected_result = ['+', '*', '*', '*', '*', '+', '*', '*', '*', '*']
     assert bool_result is False
     assert result == expected_result
 
 # PALABRA CON ACIERTOS, FALLOS Y DESORDENADA
 def test_checkWord_3():
-    word1 = "HHHHHHJJ"
-    word2 = "HHHJJJQQ"
-    bool_result, result = checkWord(word1, word2)
+    correct_word = "HHHHHHJJ"
+    user_word = "HHHJJJQQ"
+    bool_result, result = checkWord(correct_word, user_word)
     expected_result = ['+', '+', '+', '*', '*', '-', '-', '-']
     assert bool_result is False
     assert result == expected_result
     
 #  PALABRA INCORRECTA
 def test_checkWord_4():
-    word1 = "ASDF"
-    word2 = "GHJK"
-    bool_result, result = checkWord(word1, word2)
+    correct_word = "ASDF"
+    user_word = "GHJK"
+    bool_result, result = checkWord(correct_word, user_word)
     expected_result = ['-', '-', '-', '-']
     assert bool_result is False
     assert result == expected_result
 
 #  PALABRA CORRECTA
 def test_checkWord_5():
-    word1 = "MASTERWORD"
-    word2 = "MASTERWORD"
-    bool_result, result = checkWord(word1, word2)
+    correct_word = "MASTERWORD"
+    user_word = "MASTERWORD"
+    bool_result, result = checkWord(correct_word, user_word)
     expected_result = ['+', '+', '+', '+', '+', '+', '+', '+', '+', '+']
     assert bool_result is True
     assert result == expected_result
+
+
+#  PALABRA CORTA
+def test_checkLong_1():
+    correct_word = "PALABRA"
+    user_word = "PALA"
+    result = checkLong(correct_word, user_word)
+    assert result is False
+
+#  PALABRA LARGA
+def test_checkLong_2():
+    correct_word = "PALABRA"
+    user_word = "PALABRABRA"
+    result = checkLong(correct_word, user_word)
+    assert result is False
+
+#  PALABRA CORRECTA
+def test_checkLong_3():
+    correct_word = "PALABRA"
+    user_word = "AAAAAAA"
+    result = checkLong(correct_word, user_word)
+    assert result is True
+
+
 
 
 '''def test_dictionary_checkLong_1():
