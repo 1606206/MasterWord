fa909c4f551f6b192e44cb683eae124add15b292
jaesmoris <javi.esmoris@gmail.com>
2023-11-05 23:50:08 +0100

    refactor del init de Player, excepciones en los setters

--- src/controlador/classPlayer.py
+++ src/controlador/classPlayer.py
@@ -5,21 +5,9 @@ from model import *
 ## clase Jugador
 class Player: 
     def __init__(self, name="anonymous", points=0, ranking=0):
-        if not isinstance(name, str):
-            raise TypeError("Has d'introduir un string.")
-        if len(name) <= 0:
-            raise ValueError("El nom no pot estar buit.")
-        if not isinstance(points, int):
-            raise TypeError("La puntuació només pot ser un enter.")
-        if points < 0:
-            raise ValueError("La puntuació no pot ser negativa.")
-        if not isinstance(ranking, int):
-            raise TypeError("El ranking només pot ser un enter.")
-        if ranking < 0:
-            raise ValueError("El ranking no pot ser negatiu.")
-        self.name = name
-        self.points = points
-        self.ranking = ranking
+        self.set_name(name)
+        self.set_points(points)
+        self.set_ranking(ranking)
 
     # Getter para el atributo 'name'
     def get_name(self):
@@ -39,8 +27,10 @@ class Player:
 
     # Setter para el atributo 'points'
     def set_points(self, points):
-        if not (isinstance(points, int) or isinstance(points, float)):
-            raise TypeError("La puntuació no és un número.")
+        if not isinstance(points, int):
+            raise TypeError("La puntuació només pot ser un enter.")
+        if points < 0:
+            raise ValueError("La puntuació no pot ser negativa.")
         self.points = points
 
     # Getter para el atributo 'ranking'
@@ -51,6 +41,8 @@ class Player:
     def set_ranking(self, ranking):
         if not isinstance(ranking, int):
             raise TypeError("El ranking només pot ser un enter.")
-        self._ranking = ranking
+        if ranking < 0:
+            raise ValueError("El ranking no pot ser negatiu.")
+        self.ranking = ranking
 
 
