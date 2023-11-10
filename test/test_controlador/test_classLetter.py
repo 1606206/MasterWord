a2c53eb05c4dd8cb21afa850ff4752553a54f885
sereenaasg <serenasanchezgarcia@gmail.com>
2023-11-10 13:22:20 +0100

    Loop testing letter

--- test/test_controlador/test_classLetter.py
+++ test/test_controlador/test_classLetter.py
@@ -1,37 +1,38 @@
 import sys
-sys.path.insert(1,'src/controlador')
-
+sys.path.insert(2,'src/controlador')
 from classLetter import Letter
 
 #------------------------TDD-----------------------#
 
 ##------------------Loop testing---------------------##
-def test_selectColors():
-    # Caso de prueba: Todos los aciertos
-    wordList = ['A', 'B', 'C']
-    wordStatus = ['+', '+', '+']
+def test_selectColors_1(wordList = ['A', 'B', 'C'], wordStatus = []): #evitar loop
+    selectColors(wordList, wordStatus)
+    assert all(letter.color is None for letter in wordList)
+
+def test_selectColors_2(wordList = ['A', 'B', 'C'], wordStatus = ['-']): #  1 pasada
+    selectColors(wordList, wordStatus)
+    assert wordList[0].color == "fallo"
+    assert all(letter.color is None for letter in wordList[1:])
+
+
+def test_selectColors_3(wordList = ['A', 'B', 'C'], wordStatus = ['+', '-']): # 2 pasades
+    selectColors(wordList, wordStatus)
+    assert wordList[0].color == "acierto"
+    assert wordList[1].color == "mal_posicionada"
+    assert all(letter.color is None for letter in wordList[2:])
+
+def test_selectColors_4(wordList = ['A', 'B', 'C'], wordStatus = ['+']*2): # m<max pasades
+    selectColors(wordList, wordStatus)
+    assert all(letter.color == "acierto" for letter in wordList[:2])
+    assert all(letter.color is None for letter in wordList[2:])
+
+def test_selectColors_5(wordList = ['A', 'B', 'C'], wordStatus = ['+'] * (3 - 1)): # max-1 pasades
+    selectColors(wordList, wordStatus)
+    assert all(letter.color == "acierto" for letter in wordList[:-1])
+    assert wordList[-1].color is None
+
+def test_selectColors_6(wordList = ['A', 'B', 'C'], wordStatus = ['+'] * (3)): # max pasades
     selectColors(wordList, wordStatus)
     assert all(letter.color == "acierto" for letter in wordList)
 
-    # Caso de prueba: Todas las mal posicionadas
-    wordList = ['A', 'B', 'C']
-    wordStatus = ['*', '*', '*']
-    selectColors(wordList, wordStatus)
-    assert all(letter.color == "mal_posicionada" for letter in wordList)
-
-    # Caso de prueba: Todas las fallas
-    wordList = ['A', 'B', 'C']
-    wordStatus = ['-', '-', '-']
-    selectColors(wordList, wordStatus)
-    assert all(letter.color == "fallo" for letter in wordList)
-
-    # Caso de prueba: Combinación de aciertos, mal posicionadas y fallas
-    wordList = ['A', 'B', 'C']
-    wordStatus = ['+', '*', '-']
-    selectColors(wordList, wordStatus)
-    
-    # Verificar el estado esperado para cada letra
-    assert wordList[0].color == "acierto"
-    assert wordList[1].color == "mal_posicionada"
-    assert wordList[2].color == "fallo"
 
